3389818ece41aafdc64a23baa6c484576dc66634
AlexsanderRibeiroConsultor <contatoaleconsultor@gmail.com>
2026-04-29 16:34:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI, Request
 
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "ok"}
+
+@app.get("/login")
+def login():
+    return {"msg": "login funcionando"}
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    data = await request.json()
+    return {"ok": True}
