c9e63ad74b61128ce3cba73bfe8df0d2d9cf4036
AlexsanderRibeiroConsultor <contatoaleconsultor@gmail.com>
2026-04-29 16:53:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 CLIENT_ID = "8950442066920082"
 CLIENT_SECRET = "seu secret aqui"
 REDIRECT_URI = "https://ml-backend-kndd.onrender.com/callback"
+@app.get("/me")
+def me():
+    token = "COLE_SEU_ACCESS_TOKEN_AQUI"
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    response = requests.get("https://api.mercadolibre.com/users/me", headers=headers)
+    return response.json()
