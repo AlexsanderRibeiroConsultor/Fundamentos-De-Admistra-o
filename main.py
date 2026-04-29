32388ce3d0ce55789b734d4a7b10fca19a9a47ba
AlexsanderRibeiroConsultor <contatoaleconsultor@gmail.com>
2026-04-29 16:44:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+import requests
 
 app = FastAPI()
+
+CLIENT_ID = "8950442066920082"
+CLIENT_SECRET = "Pqce6DEJ6EWxGw1C2WFufCek3MflBueG"
+REDIRECT_URI = "https://ml-backend-kndd.onrender.com/callback"
 
 @app.get("/")
 def home():
@@ -8,7 +14,28 @@ def home():
 
 @app.get("/login")
 def login():
-    return {"msg": "login funcionando"}
+    url = f"https://auth.mercadolivre.com.br/authorization?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}"
+    return RedirectResponse(url)
+
+@app.get("/callback")
+def callback(code: str):
+    url = "https://api.mercadolibre.com/oauth/token"
+
+    payload = {
+        "grant_type": "authorization_code",
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "code": code,
+        "redirect_uri": REDIRECT_URI
+    }
+
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/x-www-form-urlencoded"
+    }
+
+    response = requests.post(url, data=payload, headers=headers)
+    return response.json()
 
 @app.post("/webhook")
 async def webhook(request: Request):
